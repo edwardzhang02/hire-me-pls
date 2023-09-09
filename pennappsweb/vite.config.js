4559968ca9a1f852da636fd931d8c0a1e9f3f22f
lucasz-hu <laz51@pitt.edu>
2023-09-09 14:00:36 -0400

    netlify

--- pennappsweb/vite.config.js
+++ pennappsweb/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://hiremepls.us/",
+  base: "http://hiremepls.us/",
   plugins: [react()],
   server: {
     proxy: {
