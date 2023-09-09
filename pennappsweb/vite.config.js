76773868ee6f13fbd1cfa4dde1968372d09e1d60
lucasz-hu <laz51@pitt.edu>
2023-09-09 13:43:05 -0400

    netlify

--- pennappsweb/vite.config.js
+++ pennappsweb/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://api.prospeo.io",
+        target: "https://api.prospeo.io/",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
         secure: false,
